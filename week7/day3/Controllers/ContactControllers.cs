9622abc71a26e81eecc059c8daa4120816c4d48b
kajanavaju283 <chittemsettykajanavaju283@gmail.com>
2026-04-16 16:29:55 +0530

    Add files via upload

---
+++ week7/day3/Controllers/ContactControllers.cs
@@ -0,0 +1,30 @@
+﻿using ContactApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class ContactController : Controller
+{
+    static List<ContactInfo> contacts = new List<ContactInfo>();
+
+    public IActionResult ShowContacts()
+    {
+        return View(contacts);
+    }
+
+    public IActionResult GetContactById(int id)
+    {
+        var contact = contacts.FirstOrDefault(c => c.ContactId == id);
+        return View(contact);
+    }
+
+    public IActionResult AddContacts()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult AddContacts(ContactInfo contactInfo)
+    {
+        contacts.Add(contactInfo);
+        return RedirectToAction("ShowContacts");
+    }
+}
