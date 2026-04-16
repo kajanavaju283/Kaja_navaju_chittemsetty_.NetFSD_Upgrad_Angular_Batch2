1cd2229a8320897042685d670d0209245a64d098
kajanavaju283 <chittemsettykajanavaju283@gmail.com>
2026-04-16 16:44:46 +0530

    Add files via upload

---
+++ week7/day2/Controller/CalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Week7_day2_handson.Controllers
+{
+    [Route("calculator")]
+    public class CalculatorController : Controller
+    {
+        [HttpGet("")]
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: handle form submission
+        [HttpPost("add")]
+        public IActionResult Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+
+            // store result using ViewData
+            ViewData["Result"] = result;
+
+            // return same view with result
+            return View("Index");
+        }
+    }
+}
