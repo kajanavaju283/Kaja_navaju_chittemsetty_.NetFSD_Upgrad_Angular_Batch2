8554a7c8559f0b90f224f48f8e5c3eaac0fa16ae
kajanavaju283 <chittemsettykajanavaju283@gmail.com>
2026-04-16 16:36:25 +0530

    Delete week7 directory

--- week7/day1/DataAccess/IContactRepo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AppDemo.DataAccess
-{
-    public interface IContactRepo<TEntity>
-    {
-        List<TEntity> ShowContacts();
-        TEntity GetContactById(int id);
-        bool AddContact(TEntity entity);
-    }
-}
--- week7/day2/Controller/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Week7_day2_handson.Controllers
-{
-    public class Product
-    {
-        public string Name {  get; set; }
-        public int Price {  get; set; }
-        public int Quantity {  get; set; }
-    }
-}
