478a0edda9694e397dfdb81aec17cd207b580383
kajanavaju283 <chittemsettykajanavaju283@gmail.com>
2026-04-17 10:47:35 +0530

    Add files via upload

---
+++ week8/day1,2/Controllers/ContactController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DataAccessLayer.Models;
+using DataAccessLayer.Repositories;
+
+namespace AppUILayer.Controllers
+{
+    [Route("contact")]
+    public class ContactController : Controller
+    {
+        private readonly IContactRepository _repo;
+
+        public ContactController(IContactRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("list")]
+        public IActionResult ShowContacts()
+        {
+            var data = _repo.GetAllContacts();
+            return View(data);
+        }
+
+        [HttpGet("add")]
+        public IActionResult AddContact()
+        {
+            ViewBag.Companies = _repo.GetCompanies();
+            ViewBag.Departments = _repo.GetDepartments();
+            return View();
+        }
+
+        [HttpPost("add")]
+        public IActionResult AddContact(ContactInfo contact)
+        {
+            _repo.AddContact(contact);
+            return RedirectToAction("ShowContacts");
+        }
+
+        [HttpGet("edit/{id}")]
+        public IActionResult EditContact(int id)
+        {
+            var data = _repo.GetContactById(id);
+            ViewBag.Companies = _repo.GetCompanies();
+            ViewBag.Departments = _repo.GetDepartments();
+            return View(data);
+        }
+
+        [HttpPost("edit")]
+        public IActionResult EditContact(ContactInfo contact)
+        {
+            _repo.UpdateContact(contact);
+            return RedirectToAction("ShowContacts");
+        }
+
+        [HttpGet("delete/{id}")]
+        public IActionResult DeleteContact(int id)
+        {
+            _repo.DeleteContact(id);
+            return RedirectToAction("ShowContacts");
+        }
+    }
+}
